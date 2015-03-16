c1e4892456cd4bdce4e5a59e50f14a1cf8f97082
Spharian <s.fertons@gmail.com>
2015-03-16 22:41:07 +0100

    Renamed home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'projects#index'
+
+  resources :projects, only: [:index, :show]
   
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
