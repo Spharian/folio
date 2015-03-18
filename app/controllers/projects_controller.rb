da357e6067ce42f984166b0c79b28aab85084508
Spharian <s.fertons@gmail.com>
2015-03-18 01:15:12 +0100

    Cleaned queries

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,6 @@
 class ProjectsController < ApplicationController
   def index
     @projects = Project.includes(:category)
-
-    categories_with_projects = @projects.map { |project| project.category.id }.uniq
-    @categories = Category.find(categories_with_projects)
+    @categories = @projects.map(&:category).uniq
   end
 end
