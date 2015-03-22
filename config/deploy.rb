b28b0631e9b850f32c8b2727b7c3afbdf1d5940d
Spharian <s.fertons@gmail.com>
2015-03-22 02:19:15 +0100

    Migration tests

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,30 +28,16 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-
-desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-    execute "service thin restart"  ## -> line you should add
-  end
-end
-# after :publishing, :restart
+after "deploy", "deploy:migrate"
 
 namespace :deploy do
 
-  # after :restart, :clear_cache do
-    # on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    # end
-  # end
-
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      within release_path do
+        execute :rake, 'cache:clear'
+        execute "service thin restart"
+      end
+    end
+  end
 end
