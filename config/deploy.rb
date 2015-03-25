976b11705711210fccc55ca93b335f1290723edd
Spharian <s.fertons@gmail.com>
2015-03-25 22:31:06 +0100

    New deploy files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,49 +1,51 @@
-# config valid only for current version of Capistrano
-lock '3.4.0'
+set :application, "folio"
+set :deploy_user, "deployer"
 
-set :application, 'folio'
-set :repo_url, 'git@github.com:Spharian/folio.git'
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :deploy_to, "/var/www/#{fetch(:application)}"
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
+# Repo details
+set :repo_url, "git@github.com:Spharian/folio.git"
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push("config/database.yml", "config/secrets.yml")
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system")
+
+# Which config files should be copied by deploy:setup_config
+set(:config_files, %w(
+  nginx.conf
+  database.example.yml
+  unicorn.rb
+  unicorn_init.sh
+))
+
+# Which config files should be made executable after copying
+# by deploy:setup_config
+set(:executable_config_files, %w(
+  unicorn_init.sh
+))
+
+# Files which need to be symlinked to other parts of the
+# filesystem. For example nginx virtualhosts,
+# init scripts etc. The full_app_name variable isn't
+# available at this point so we use a custom template {{}}
+# tag and then add it at run time.
+set(:symlinks, [
+  {
+    source: "nginx.conf",
+    link: "/etc/nginx/sites-enabled/{{full_app_name}}"
+  },
+  {
+    source: "unicorn_init.sh",
+    link: "/etc/init.d/unicorn_{{full_app_name}}"
+  }
+])
 
 namespace :deploy do
-  # Custom tasks (cap -T)
-  desc "Create nginx symlink"
-  task :nginx_symlink do
-    on roles(:app) do
-      execute "ln -s -f /var/www/#{fetch(:application)}/current/config/nginx.conf /etc/nginx/sites-enabled/#{fetch(:application)}"
-    end
-  end
+  # Remove the default nginx configuration as it will tend
+  # to conflict with our configs.
+  before "deploy:setup_config", "nginx:remove_default_vhost"
 
-  desc "Create unicorn init symlink"
-  task :unicorn_init_symlink do
-    on roles(:app) do
-      # Make unicorn_init.sh executable
-      execute "chmod +x /var/www/#{fetch(:application)}/current/config/unicorn_init.sh"
-      # Create the symlink
-      execute "ln -s -f /var/www/#{fetch(:application)}/current/config/unicorn_init.sh /etc/init.d/unicorn-#{fetch(:application)}"
-    end
-  end
-
-  after :finished, 'deploy:nginx_symlink'
-  after :finished, 'deploy:unicorn_init_symlink'
+  # Reload nginx to it will pick up any modified vhosts from
+  # setup_config
+  after "deploy:setup_config", "nginx:reload"
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,59 +1,18 @@
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
+# This is used in the Nginx VirtualHost to specify which domains
+# the app should appear on. If you don't yet have DNS setup, you'll
+# need to create entries in your local Hosts file for testing.
+set :server_name, "www.ketfikevin.ddns.net ketfikevin.ddns.net"
 
-server '92.222.14.113', user: 'deployer', roles: %w{app web db}
+server "92.222.14.113", user: "#{fetch(:deploy_user)}", roles: %w{app web db}
 
+set :deploy_to, "/var/www/#{fetch(:application)}"
 
+set :rails_env, :production
 
-# role-based syntax
-# ==================
+# Number of unicorn workers, this will be reflected in
+# the unicorn.rb and the monit configs
+set :unicorn_worker_count, 2
 
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any  hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
-
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
-
-
-
-# Configuration
-# =============
-# You can set any configuration variable like in config/deploy.rb
-# These variables are then only loaded and set in this stage.
-# For available Capistrano configuration variables see the documentation page.
-# http://capistranorb.com/documentation/getting-started/configuration/
-# Feel free to add new variables to customise your setup.
-
-
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+# Whether we're using ssl or not, used for building nginx
+# config file
+set :enable_ssl, false
