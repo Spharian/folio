fc7f25e7818bde3f3f33123c52e19f2859da598f
Spharian <s.fertons@gmail.com>
2018-02-11 09:58:10 +0100

    Use before_action with condition

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_filter :redirect_heroku_domain
+  before_action :redirect_heroku_domain, if: proc { app_url && heroku_url }
   protect_from_forgery with: :exception
 
   private
 
     def redirect_heroku_domain
-      return unless app_url && heroku_url
       redirect_to app_url if request.host.include?(heroku_url)
     end
 
