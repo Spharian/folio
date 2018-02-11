0187e09c5902b2e59cf39dfff43761c6cfbb6bbf
Spharian <s.fertons@gmail.com>
2018-02-11 09:48:37 +0100

    Use env variables for heroku domain redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,18 @@ class ApplicationController < ActionController::Base
   before_filter :redirect_heroku_domain
   protect_from_forgery with: :exception
 
-  def redirect_heroku_domain
-    if request.host.include?('ketfikevin.herokuapp.com')
-      redirect_to 'http://ketfikevin.com'
+  private
+
+    def redirect_heroku_domain
+      return unless app_url && heroku_url
+      redirect_to app_url if request.host.include?(heroku_url)
     end
-  end
+
+    def app_url
+      @app_url ||= ENV['APP_URL']
+    end
+
+    def heroku_url
+      @heroku_url ||= ENV['HEROKU_URL']
+    end
 end
